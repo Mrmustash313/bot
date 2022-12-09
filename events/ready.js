4f1eaecfa4ad70cc41700440286172f988647b43
Mrmustash313 <thomasmiken212@gmail.com>
2022-12-08 20:04:38 -0600

    Initial commit

---
+++ events/ready.js
@@ -0,0 +1,17 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const client = require('../index');
+const config = require('../config/config.json')
+/** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  client.user.setActivity(`${config.prefix}help`)
+})
+
+
+
